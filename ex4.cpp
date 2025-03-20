e6cb5c1e76f86aab051d222ac20f48aa1e56b91f
KOAS <khoaradequa@gmail.com>
2025-03-20 14:25:13 +0700

    ex4-Tower of Hanoi

--- ex4.cpp
+++ ex4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void towerOfHanoi(int n,char from, char to, char aux){
     //the main idea is to put n-1 disk to the aux-nod and then put the n disk to to-nod,
-    //we divide the problem to smaller problem by recursion and solve it from 1 disk problem to n disks problem
+    //we divide the problem to smaller problem by recursion and solve it from 1 disk problem to n disks problem.
     if(n==1){
         cout<<"Moved disk 1 from "<<from<<" to "<<to<<endl;
         return;
