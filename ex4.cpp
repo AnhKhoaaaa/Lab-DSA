847e18388b60b7916728d69c9ac4fb993ce9801a
KOAS <khoaradequa@gmail.com>
2025-03-20 12:39:37 +0700

    Added ex4.cpp
    completed ex4-Tower of Hanoi

---
+++ ex4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void towerOfHanoi(int n,char from, char to, char aux){
+    if(n==1){
+        cout<<"Move disk 1 from "<<from<<" to "<<to<<endl;
+        return;
+    }
+    towerOfHanoi(n-1,from,aux,to);
+    cout<<"Moved disk "<<n<<" from "<<from<<" to "<<to<<endl;
+    towerOfHanoi(n-1,aux,to,from);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    char from='A';
+    char to='C';
+    char aux='B';
+    towerOfHanoi(n,from,to,aux);
+    return 0;
+}
