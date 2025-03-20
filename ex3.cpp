e5107c8d9dc91c6555b54966608e3c4a0a79fa6a
KOAS <khoaradequa@gmail.com>
2025-03-20 14:23:08 +0700

    ex3-generate Binary Strings

--- ex3.cpp
+++ ex3.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 void generateBinaryStrings(int n,string str){
     if(str.length()==n){
-        cout<<str<<endl;
+        cout<<str<<endl; //when the length of the string equal to N, print the string
         return;
     }
-    generateBinaryStrings(n,str+'0');
+    // Two recursion call to add 0 or 1 to current string, the function will add every possible
+    // 0 characters and then add 1
+    generateBinaryStrings(n,str+'0'); 
     generateBinaryStrings(n,str+'1');
 }
 
